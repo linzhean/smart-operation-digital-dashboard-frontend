4efa46e9c7c6aada9292d0d698783c085c3f6360
chengonn <11046038@ntub.edu.tw>
2024-03-22 14:57:53 +0800

    新增 - Chart圖表

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,85 @@
-import React, {useState, useEffect} from 'react';
-import {fetchData} from "./AppService";
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import DrawerNavigation from './component/DrawerNavigation';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import './App.css';
-import {inspect} from "util";
-
-export interface Item {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
-}
+import Home from './component/Home';
+import Group from './component/Group';
+// import Services from './component/Services';
+// import Email from './component/Email';
+// import Profile from './component/Profile';
 
 const App: React.FC = () => {
-    const [data, setData] = useState<Item[]>([]);
+  const [tabs, setTabs] = useState([" 群組 1"]);
+  const [users, setUsers] = useState<any[]>([]);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [selectedPage, setSelectedPage] = useState("home"); // 新增狀態來跟踪当前选中的页面，默认为"home"
 
-    useEffect(() => {
-        const fetchDataAsync = async () => {
-            try {
-                const fetchedData = await fetchData();
-                console.log(`fetchedData : ${fetchedData}`)
-                setData(fetchedData);
-            } catch (error) {
-                // Handle error
-            }
-        };
+  const addTab = () => {
+    const newTab = `群組 ${tabs.length + 1}`;
+    setTabs([...tabs, newTab]);
+  };
 
-        fetchDataAsync();
-    }, []);
+  const deleteTab = (index: number) => {
+    if (window.confirm("確定要刪除嗎？")) {
+      const newTabs = [...tabs];
+      newTabs.splice(index, 1);
+      setTabs(newTabs);
+    }
+  };
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <div>
-                    <h1>Data from Django API:</h1>
-                    <ul>
-                        {data !== undefined && data.length > 0 ?
-                            data.map(item => (
-                                <li key={item.id}>
-                                    <strong>{item.name}</strong>: {item.description}
-                                </li>
-                            ))
-                            : <li>No data available</li>
-                        }
-                    </ul>
-                </div>
-                <a href="http://localhost:8000/accounts/login/">Sign in with Google</a>
-            </header>
-        </div>
-    );
-}
+  const addUser = () => {
+    const newUser = {
+      username: "New User",
+      department: "New Department",
+      name: "New Name",
+      gmail: "New Gmail",
+      position: "New Position",
+    };
+    setUsers([...users, newUser]);
+  };
+
+  const deleteUser = (index: number) => {
+    const newUsers = [...users];
+    newUsers.splice(index, 1);
+    setUsers(newUsers);
+  };
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const selectPage = (page: string) => {
+    setSelectedPage(page);
+  };
+
+  return (
+    <div className='App'>
+      <div className="top-navigation">
+        <button className={`top-nav-item ${selectedPage === "home" ? "active" : ""}`} onClick={() => selectPage("home")}>首頁</button>
+        <button className={`top-nav-item ${selectedPage === "group" ? "active" : ""}`} onClick={() => selectPage("group")}>群組</button>
+        <button className={`top-nav-item ${selectedPage === "services" ? "active" : ""}`} onClick={() => selectPage("services")}>服務</button>
+        <button className={`top-nav-item ${selectedPage === "email" ? "active" : ""}`} onClick={() => selectPage("email")}>郵件</button>
+        <button className={`top-nav-item ${selectedPage === "profile" ? "active" : ""}`} onClick={() => selectPage("profile")}>個人資料</button>
+      </div>
+      <button className="toggle-button" onClick={toggleDrawer}>
+        {isDrawerOpen ? <FaAngleLeft /> : <FaAngleRight />}
+      </button>
+      <DrawerNavigation
+        tabs={tabs}
+        onAddTab={addTab}
+        onDeleteTab={deleteTab}
+        isOpen={isDrawerOpen}
+        toggleDrawer={toggleDrawer}
+      />
+      <div className={`content ${isDrawerOpen ? 'drawer-open' : 'drawer-closed'}`}>
+        {selectedPage === "home" && <Home />}
+        {selectedPage === "group" && <Group users={users} addUser={addUser} deleteUser={deleteUser} />}
+        {/* {selectedPage === "services" && <Services />}
+        {selectedPage === "email" && <Email />}
+        {selectedPage === "profile" && <Profile />} */}
+      </div>
+    </div>
+  );
+};
 
 export default App;
