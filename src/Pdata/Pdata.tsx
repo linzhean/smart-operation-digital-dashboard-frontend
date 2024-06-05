1590b2498a82a0416a74833be05dfd7dcb28cde3
jerrylin <jerrylin920316@gmail.com>
2024-06-06 07:55:21 +0800

    Pdata - 版面調整

--- src/Pdata/Pdata.tsx
+++ src/Pdata/Pdata.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './Pdata.css';
 import Edit from '../assets/icon/edit-icon.svg';
 
-const backendApiUrl = "http://140.131.115.153:8080";
+const backendApiUrl = "https://smart-operation-digital-dashboard.kuohao.wtf/backend";
 
 const Pdata: React.FC = () => {
   const navigate = useNavigate();
@@ -60,93 +60,88 @@ const Pdata: React.FC = () => {
   return (
     <main className="section">
       <div className="container">
-        <form className="row g-3 needs-validation" noValidate onSubmit={handleSubmit}>
-          <legend></legend>
-          <div className="col-md-12 listTitle">
-            您的個人資料
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="validationName" className="form-label">姓名</label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              value={formData.name}
-              required
-              disabled={!editable}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6"></div>
-          <div className="col-md-6">
-            <label htmlFor="validationNum" className="form-label">工號</label>
-            <input
-              type="text"
-              className="form-control"
-              id="num"
-              value={formData.num}
-              required
-              disabled={!editable}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="validationEmail" className="form-label">信箱<span id="gmail">(Gmail)</span></label>
-            <div className="input-group has-validation">
+          <form className="form-div" noValidate onSubmit={handleSubmit}>
+            <h2>您的個人資料</h2>
+            <div className="column-div">
+              <label htmlFor="validationName" className="form-label">姓名</label>
               <input
                 type="text"
                 className="form-control"
-                id="email"
-                aria-describedby="inputGroupAppend"
-                value={formData.email}
+                id="name"
+                value={formData.name}
+                required
                 disabled={!editable}
                 onChange={handleInputChange}
               />
-              <span className="input-group-text" id="inputGroupAppend">@gmail.com</span>
             </div>
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="validationUnit" className="form-label">所屬部門</label>
-            <select
-              className="form-select"
-              id="unit"
-              required
-              disabled={!editable}
-              value={formData.unit}
-              onChange={handleInputChange}
-            >
-              <option value="">...</option>
-              <option>銷售</option>
-              <option>生產</option>
-              <option>財務</option>
-              <option>審計</option>
-            </select>
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="validationRole" className="form-label">職稱</label>
-            <select
-              className="form-select"
-              id="role"
-              required
-              disabled={!editable}
-              value={formData.role}
-              onChange={handleInputChange}
-            >
-              <option value="">...</option>
-              <option>一般員工</option>
-              <option>副理</option>
-              <option>經理</option>
-            </select>
-          </div>
-          {error && <p className="error">{error}</p>}
-          <div className="col-12">
-            <button className="btn btn-primary" type="submit">提交</button>
-            <button type="button" className="btn btn-secondary" onClick={handleEditClick}>
-              <img src={Edit} alt="Edit" />
-              {editable ? '取消編輯' : '編輯'}
-            </button>
-          </div>
-        </form>
+            <div className="column-div">
+              <label htmlFor="validationNum" className="form-label">工號</label>
+              <input
+                type="text"
+                className="form-control"
+                id="num"
+                value={formData.num}
+                required
+                disabled={!editable}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="column-div">
+              <label htmlFor="validationEmail" className="form-label">信箱</label>
+              <div className="input-group has-validation">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  aria-describedby="inputGroupAppend"
+                  value={formData.email}
+                  disabled={!editable}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+            <div className="column-div">
+              <label htmlFor="validationUnit" className="form-label">所屬部門</label>
+              <select
+                className="form-select"
+                id="unit"
+                required
+                disabled={!editable}
+                value={formData.unit}
+                onChange={handleInputChange}
+              >
+                <option value="">...</option>
+                <option>銷售</option>
+                <option>生產</option>
+                <option>財務</option>
+                <option>審計</option>
+              </select>
+            </div>
+            <div className="column-div">
+              <label htmlFor="validationRole" className="form-label">職稱</label>
+              <select
+                className="form-select"
+                id="role"
+                required
+                disabled={!editable}
+                value={formData.role}
+                onChange={handleInputChange}
+              >
+                <option value="">...</option>
+                <option>一般員工</option>
+                <option>副理</option>
+                <option>經理</option>
+              </select>
+            </div>
+            {error && <p className="error">{error}</p>}
+            <div className="raw-div">
+              <button className="submit-button" type="submit">提交</button>
+              <button type="button" className="edit-button" onClick={handleEditClick}>
+                {editable ? '取消編輯' : '編輯'}
+                <img src={Edit} alt="Edit" className='img'/>
+              </button>
+            </div>
+          </form>
       </div>
     </main>
   );
