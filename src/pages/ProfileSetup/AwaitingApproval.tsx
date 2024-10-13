b256d2d5ba4d43dfb14a0bdb5bb1bc15b5ab9b57
KJ <kkaoira27@gmail.com>
2024-10-13 23:04:49 +0800

    修改：微調樣式

--- src/pages/ProfileSetup/AwaitingApproval.tsx
+++ src/pages/ProfileSetup/AwaitingApproval.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import styles from './AwaitingApproval.module.css';
 import LOGO from '../../assets/icon/Logo-GIF-crop.gif'
+
 const AwaitingApproval: React.FC = () => {
   return (
     <div className={styles.container}>
@@ -14,6 +15,8 @@ const AwaitingApproval: React.FC = () => {
         <br />
         <span className={styles.theBig}>等待管理員審核</span>
         <span className={styles.theSmall}>您的資料已經提交，請等待管理員審核。</span>
+        {/* <span className={styles.theBig}>智慧營運數位儀表板</span>
+        <span className={styles.theSmall}>神秘數智揭露未來</span> */}
       </div>
     </div>
   );
